4a29982787bbdbd344859088300dc5151f1498bf
GIULIA <brun.ilde@hotmail.com>
2023-06-08 16:13:34 +0200

    Consegna

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// Popolo il mio tabellone
+const allNum = []
+const populateTable = function(){
+    let gameTable = document.getElementById('game-table')
+    for(let i=1; i<=76; i++){
+
+        let numCells = document.createElement('div')
+        numCells.classList.add('game-cells')
+        gameTable.appendChild(numCells)
+        numCells.append(`${i}`)
+        allNum.push(i)
+    }
+    return allNum
+}
+populateTable()
+console.log(allNum)
+
+// Creo il bottone per l'estrazione
+const extractedNum = []
+const numExtraction = function(){
+    const extractionBtn = document.getElementById('extraction-btn')
+    extractionBtn.addEventListener('click', function(){
+        let randomNum = Math.floor(Math.random() * 77)
+        // Prevengo la possibilità che venga estratto un numero già uscito
+        if (extractedNum.indexOf(randomNum) === -1) {
+            extractedNum.push(randomNum)
+            return extractedNum
+        }
+    })
+}
+numExtraction()
+console.log(extractedNum)
+
+// Comparo l'array dei numeri del tabellone con quelli estratti
+const nonCommonNum = (allNum, extractedNum) => {
+    const nonCommonNum = []
+    for(let i = 0; i < allNum.length; i++){
+       if(extractedNum.indexOf(allNum[i]) === -1){
+          nonCommonNum.push(allNum[i])
+       }
+    }
+    for(let j = 0; j < extractedNum.length; j++){
+       if(allNum.indexOf(extractedNum[j]) === -1){
+          nonCommonNum.push(extractedNum[j])
+       }
+    }
+    return nonCommonNum;
+ }
+
+ console.log(nonCommonNum(allNum,extractedNum))
